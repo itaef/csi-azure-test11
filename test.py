2103ab44a8bd8ac8409fb3ea9bf6f85530c09e50
iness <inesstaef33@gmail.com>
2021-11-30 00:05:40 +0200

    adding commit

--- test.py
+++ test.py
@@ -1,5 +1,19 @@
 #!/usr/bin/python
 
-# Hello world python program
+import azure.mgmt.resource
+import automationassets
+from azure_automation_utility import get_automation_runas_credential
 
-print "Hello World!";
+# Authenticate to Azure using the Azure Automation RunAs service principal
+runas_connection = automationassets.get_automation_connection("AzureRunAsConnection")
+azure_credential = get_automation_runas_credential()
+
+# Intialize the resource management client with the RunAs credential and subscription
+resource_client = azure.mgmt.resource.ResourceManagementClient(
+    azure_credential,
+    str(runas_connection["SubscriptionId"]))
+
+# Get list of resource groups and print them out
+groups = resource_client.resource_groups.list()
+for group in groups:
+    print group.name
